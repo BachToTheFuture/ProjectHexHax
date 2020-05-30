23263bda19c083e96aff7cd39024ee42a9ad15c2
Jendy <53535722+jendyren@users.noreply.github.com>
2020-05-30 11:02:19 -0400

    HexHax-Website
    First look at map and website design

---
+++ HexHaxWebsite/map.js
@@ -0,0 +1,6 @@
+
+// The map
+var map = L.map('mapid').setView([0,0], 3);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+}).addTo(map);
