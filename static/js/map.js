b59851925c62b1c971fbdf6f8946a6f353f813d2
Bach Nguyen <bachtothefuture@Bachs-MacBook-Pro.local>
2020-05-31 13:01:28 -0500

    Changes

--- static/js/map.js
+++ static/js/map.js
@@ -15,8 +15,8 @@ L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.pn
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var southWest = L.latLng(-89.98155760646617, -180),
-northEast = L.latLng(89.99346179538875, 180);
+var southWest = L.latLng(-89.98155760646617, -1000000),
+northEast = L.latLng(89.99346179538875, 1000000);
 var bounds = L.latLngBounds(southWest, northEast);
 
 map.setMaxBounds(bounds);
@@ -41,178 +41,226 @@ setTimeout(function() {
 
 var marker = {};
 
+var bounds_geojson;
+
 map.on('click', function (info) {
 	sidebar.show();
     $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${info.latlng.lat}&lon=${info.latlng.lng}`, function(data){
-        //console.log(data);
+		// Display the outline
+	
+		console.log(data);
 		var lat = data.lat;
 		var lon = data.lon;
 
-		// add marker
-		if (marker != undefined) {
-              map.removeLayer(marker);
-        };
-		marker = L.marker([lat,lon]).addTo(map)
-		// Should I zoom in?
-		//map.setView(info.latlng, 5);
+		if (lat && lon) {
+			// add marker
+			
+			// Should I zoom in?
 
-		var country = data.address.country;
-		var state = data.address.state;
-		//<h2>${(data.address.state) ? data.address.state + ", " + data.address.country : data.address.country}</h2>
 
-		/*
-		Put these after the graphs
-		<p><b>Geographical Info</b></p>
-			<table class="table table-bordered">
-				<tbody>
-				<tr>
-					<th>Latitude</th>
-					<td>${data.lat}</td>
-				</tr>
-				<tr>
-					<th>Longitude</th>
-					<td>${data.lon}</td>
-				</tr>
-				</tbody>
-			</table>
-		*/
-		// Graphs and stuff go here
+			var country = data.address.country;
+			var state = data.address.state;
+			//<h2>${(data.address.state) ? data.address.state + ", " + data.address.country : data.address.country}</h2>
 
-		//var data = ["new_cases" for x in covid_data[country]];
+			/*
+			Put these after the graphs
+			<p><b>Geographical Info</b></p>
+				<table class="table table-bordered">
+					<tbody>
+					<tr>
+						<th>Latitude</th>
+						<td>${data.lat}</td>
+					</tr>
+					<tr>
+						<th>Longitude</th>
+						<td>${data.lon}</td>
+					</tr>
+					</tbody>
+				</table>
+			*/
+			// Graphs and stuff go here
 
-    if (state_covid_cases[state] && state_covid_deaths[state]){
-      $("#infobar").html(`
-            <h2>${data.address.state + ", " + data.address.country}</h2>
-			<hr>
-			<canvas id="covid-chart-new" width="800" height="800"></canvas>
-			<canvas id="covid-chart-totals" width="800" height="800"></canvas>`);
-      //console.log(state_covid_cases[state])
-      let total_cases = []
-      let total_deaths = []
-      let dates = []
-      for (var n = 0; n < state_covid_cases[state].length; n++) {
-        total_cases.push(state_covid_cases[state][n]["total_cases"]);
-        dates.push(state_covid_cases[state][n]["date"]);
-      }
-
-      for (var m = 0; m < state_covid_deaths[state].length; m++) {
-        total_deaths.push(state_covid_deaths[state][m]["total_deaths"]);
-        //dates.push(state_covid_deaths[state][n]["date"]);
-      }
-
-      console.log(total_deaths)
-      new Chart(document.getElementById("covid-chart-new"), {
-          type: 'line',
-          data: {
-          labels: dates,
-          datasets: [{
-            data: total_cases,
-            label: "Total Cases",
-            borderColor: "#3e95cd",
-            fill: false
-            }, {
-            data: total_deaths,
-            label: "Total Deaths",
-            borderColor: "#000000",
-            fill: false
-          }
-          ]
-          },
-          options: {
-          title: {
-            display: true,
-            text: 'COVID-19 New Cases and Deaths for '+ state
-          }
-          }
-        });
-    }
-
-    // Show country data otherwise if state data is undefined
-	else if (covid_data[country]) {
-		$("#infobar").html(`
-            <h2>${data.address.country}</h2>
-			<hr>
-			<canvas id="covid-chart-new" width="800" height="800"></canvas>
-			<canvas id="covid-chart-totals" width="800" height="800"></canvas>
-        `);
-			//date,location,new_cases,new_deaths,total_cases,total_deaths
-			let new_cases = []
-			let new_deaths = []
+			//var data = ["new_cases" for x in covid_data[country]];
+		
+		if (state_covid_cases[state] && state_covid_deaths[state]){
+			// Loading screen
+			$("#infobar").html(`
+				<h2>${state + ", " + country}</h2>
+				<hr>
+				<div class="cssload-container">
+				<ul class="cssload-flex-container">
+					<li>
+						<span class="cssload-loading cssload-one"></span>
+						<span class="cssload-loading cssload-two"></span>
+						<span class="cssload-loading-center"></span>
+					</li>
+				</div>
+			</div>`);
+				
+			$.get(`http://nominatim.openstreetmap.org/search?q=${data.address.state + "+" + data.address.country}&polygon_geojson=1&format=json`, function(data) {
+				console.log(data);
+				if (bounds_geojson !== undefined) map.removeLayer(bounds_geojson);
+				bounds_geojson = L.geoJson(data[0].geojson, {
+					style: style,
+					click: zoomToFeature
+				}).addTo(map);
+				$("#infobar").html(`
+				<h2>${state + ", " + country}</h2>
+				<hr>
+				<canvas id="covid-chart-new" width="800" height="800"></canvas>
+				<canvas id="covid-chart-totals" width="800" height="800"></canvas>`);
+			//console.log(state_covid_cases[state])
 			let total_cases = []
 			let total_deaths = []
 			let dates = []
-
-			for (var n = 0; n < covid_data[country].length; n++) {
-				new_cases.push(covid_data[country][n]["new_cases"]);
-				new_deaths.push(covid_data[country][n]["new_deaths"]);
-				total_cases.push(covid_data[country][n]["total_cases"]);
-				total_deaths.push(covid_data[country][n]["total_deaths"]);
-				dates.push(covid_data[country][n]["date"]);
+			for (var n = 0; n < state_covid_cases[state].length; n++) {
+			total_cases.push(state_covid_cases[state][n]["total_cases"]);
+			dates.push(state_covid_cases[state][n]["date"]);
 			}
-			//console.log(new_cases);
-			//console.log(dates);
-		
+
+			for (var m = 0; m < state_covid_deaths[state].length; m++) {
+			total_deaths.push(state_covid_deaths[state][m]["total_deaths"]);
+			//dates.push(state_covid_deaths[state][n]["date"]);
+			}
+
+			console.log(total_deaths)
 			new Chart(document.getElementById("covid-chart-new"), {
 				type: 'line',
 				data: {
 				labels: dates,
 				datasets: [{
-					data: new_cases,
-					label: "New Cases",
-					borderColor: "#63d13e",
-					fill: false
-					}, {
-					data: new_deaths,
-					label: "New Deaths",
-					borderColor: "#ff3e33",
-					fill: false
-					}
+				data: total_cases,
+				label: "Total Cases",
+				borderColor: "#3e95cd",
+				fill: false
+				}, {
+				data: total_deaths,
+				label: "Total Deaths",
+				borderColor: "#000000",
+				fill: false
+				}
 				]
 				},
 				options: {
 				title: {
-					display: true,
-					text: 'COVID-19 New Cases and Deaths for '+country
+				display: true,
+				text: 'COVID-19 New Cases and Deaths for '+ state
 				}
 				}
 			});
-				
-			//end chart
-
-			new Chart(document.getElementById("covid-chart-totals"), {
-				type: 'line',
-				data: {
-				labels: dates,
-				datasets: [{
-					data: total_cases,
-					label: "Total Cases",
-					borderColor: "#3e95cd",
-					fill: false
-					}, {
-					data: total_deaths,
-					label: "Total Deaths",
-					borderColor: "#000000",
-					fill: false
-					}
-				]
-				},
-				options: {
-				title: {
-					display: true,
-					text: 'COVID-19 Total Cases and Deaths for '+country
-					}
-				}
 			});
-			// end
+			
 		}
-    else {
-      $("#infobar").html(`
-            <h2>${data.address.country}</h2>
-			<hr>
-			<p><b>No Data</b></p>
-      `);
-    }
 
+		// Show country data otherwise if state data is undefined
+		else if (covid_data[country]) {
+			$("#infobar").html(`
+				<h2>${country}</h2>
+				<hr>
+				<div class="cssload-container">
+				<ul class="cssload-flex-container">
+					<li>
+						<span class="cssload-loading cssload-one"></span>
+						<span class="cssload-loading cssload-two"></span>
+						<span class="cssload-loading-center"></span>
+					</li>
+				</div>
+			</div>`);
+
+			$.get(`http://nominatim.openstreetmap.org/search?country=${data.address.country}&polygon_geojson=1&format=json`, function(data) {
+				console.log(data);
+				if (bounds_geojson !== undefined) map.removeLayer(bounds_geojson);
+				bounds_geojson = L.geoJson(data[0].geojson, {
+					style: style,
+				}).addTo(map);
+				map.fitBounds(bounds_geojson.getBounds());
+
+				$("#infobar").html(`
+				<h2>${country}</h2>
+				<hr>
+				<canvas id="covid-chart-new" width="800" height="800"></canvas>
+				<canvas id="covid-chart-totals" width="800" height="800"></canvas>
+			`);
+				//date,location,new_cases,new_deaths,total_cases,total_deaths
+				let new_cases = []
+				let new_deaths = []
+				let total_cases = []
+				let total_deaths = []
+				let dates = []
+
+				for (var n = 0; n < covid_data[country].length; n++) {
+					new_cases.push(covid_data[country][n]["new_cases"]);
+					new_deaths.push(covid_data[country][n]["new_deaths"]);
+					total_cases.push(covid_data[country][n]["total_cases"]);
+					total_deaths.push(covid_data[country][n]["total_deaths"]);
+					dates.push(covid_data[country][n]["date"]);
+				}
+				//console.log(new_cases);
+				//console.log(dates);
+			
+				new Chart(document.getElementById("covid-chart-new"), {
+					type: 'line',
+					data: {
+					labels: dates,
+					datasets: [{
+						data: new_cases,
+						label: "New Cases",
+						borderColor: "#63d13e",
+						fill: false
+						}, {
+						data: new_deaths,
+						label: "New Deaths",
+						borderColor: "#ff3e33",
+						fill: false
+						}
+					]
+					},
+					options: {
+					title: {
+						display: true,
+						text: 'COVID-19 New Cases and Deaths for '+country
+					}
+					}
+				});
+					
+				//end chart
+
+				new Chart(document.getElementById("covid-chart-totals"), {
+					type: 'line',
+					data: {
+					labels: dates,
+					datasets: [{
+						data: total_cases,
+						label: "Total Cases",
+						borderColor: "#3e95cd",
+						fill: false
+						}, {
+						data: total_deaths,
+						label: "Total Deaths",
+						borderColor: "#000000",
+						fill: false
+						}
+					]
+					},
+					options: {
+					title: {
+						display: true,
+						text: 'COVID-19 Total Cases and Deaths for '+country
+						}
+					}
+				});
+			});
+			
+				// end
+			}
+		else {
+			$("#infobar").html(`
+				<h2>${data.address.country}</h2>
+				<hr>
+				<p><b>No Data</b></p>
+			`);
+		}
+		}
     });
 });
 //////////////////
@@ -235,10 +283,10 @@ function style(feature) {
 	return {
 		weight: 2,
 		opacity: 1,
-		color: getColor(feature.properties.density),
-		dashArray: '',
-		fillOpacity: 0,
-		//fillColor: getColor(feature.properties.density)
+		color: 'white',
+		dashArray: '3',
+		fillOpacity: 0.7,
+		fillColor: getColor(feature.properties.density)
 	};
 }
 
@@ -249,8 +297,7 @@ function highlightFeature(e) {
 		weight: 5,
 		color: '#666',
 		dashArray: '',
-		fillOpacity: 0.7,
-		fillColor: "white"
+		fillOpacity: 0.7
 	});
 
 	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
@@ -272,27 +319,18 @@ function onEachFeature(feature, layer) {
 	layer.on({
 		mouseover: highlightFeature,
 		mouseout: resetHighlight,
-		click: zoomToFeature
+		//click: zoomToFeature
 	});
 }
 
-
+/*
 geojson = L.geoJson(statesData, {
 	style: style,
 	onEachFeature: onEachFeature
 }).addTo(map);
+*/
 
-geojson = L.geoJson(statesData, {
-	style: style,
-	onEachFeature: onEachFeature
-}).addTo(map);
-
-geojson = L.geoJson(countries_data, {
-	style: style,
-	onEachFeature: onEachFeature
-}).addTo(map);
-
-
+/*
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (map) {
@@ -316,3 +354,4 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(map);
+*/
